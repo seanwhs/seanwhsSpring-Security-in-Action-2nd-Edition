e30dc9da863d9e14910ab1bf4b167f76518b09b0
Sean <seanwhs@gmail.com>
2024-01-29 12:23:15 +0800

    Implemented JdbcUserDetailsManager and MySQL for user management

---
+++ Chapter 3 ex2/exercise2/src/main/java/chapter3/exercise2/Exercise1Application.java
@@ -0,0 +1,13 @@
+// Exercise1Application.java
+package chapter3.exercise2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise1Application.class, args);
+	}
+
+}
