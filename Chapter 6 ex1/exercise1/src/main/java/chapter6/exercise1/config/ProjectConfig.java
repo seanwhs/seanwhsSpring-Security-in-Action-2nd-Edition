278ec45f1c0adb741316b92f5c1b667cff9da354
Sean <seanwhs@gmail.com>
2024-01-30 14:26:51 +0800

    Implemented Customized Authentication Provider

---
+++ Chapter 6 ex1/exercise1/src/main/java/chapter6/exercise1/config/ProjectConfig.java
@@ -0,0 +1,40 @@
+// ProjectConfig.java
+package chapter6.exercise1.config;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class ProjectConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public ProjectConfig(
+            AuthenticationProvider authenticationProvider,
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        this.authenticationProvider = authenticationProvider;
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(authenticationProvider);
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        return http.build();
+    }
+}
